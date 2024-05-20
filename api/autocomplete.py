227865ebf4f4bd87ed4ba39bfd2863b6592279c7
jiwon <wldnjstj99@gmail.com>
2024-05-21 04:57:54 +0900

    초성검색 자음/모음 개별 탐색 추가

--- api/autocomplete.py
+++ api/autocomplete.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from jamo import *
+
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+BASE_CODE, CHOSUNG, JUNGSUNG = 44032, 588, 28
 
 class Node:
     def __init__(self, key, data=None):
@@ -6,20 +10,114 @@ class Node:
         self.data = []
         self.children = {}
 
-def returnTrie():
-    df = pd.read_csv("./brands.csv")
-
-    brand_list = df.iterrows()
-    head = Node(None)
-    for i, brand in brand_list:
-        kor_name = brand["kor_name"]
-        current_node = head
-        for kor in kor_name:
-            if kor not in current_node.children:
-                current_node.children[kor] = Node(kor)
-                current_node.children[kor].data.append(kor_name)
+class SubjectName:
+    def __init__(self) -> None:
+        self.trie = self.returnTrie_ver2()
+        
+    def return_jamo(self, text):
+        return_value = ""
+        for t in text:
+            jamo_list = j2hcj(h2j(t))
+            if len(jamo_list) == 2:
+                return_value += jamo_list[0]
+                return_value += j2h(*jamo_list)
             else:
+                return_value += jamo_list[0]
+                for i in range(2,len(jamo_list)+1):
+                    han = j2h(*jamo_list[:i])
+                    return_value += han
+        return return_value
+    
+    def return_chosung(self, text):
+        chosung = ""
+        for t in text:
+            if ord('가') <= ord(t) <= ord('힣'):
+                chosung_index = (ord(t) - BASE_CODE) // CHOSUNG
+                chosung += CHOSUNG_LIST[chosung_index]
+            else:
+                chosung += t
+        return chosung
+
+    def returnTrie_ver2(self):
+        df = pd.read_csv(r"C:\Users\wldnj\OneDrive\바탕 화면\autocomplete\brands.csv")
+        brand_list = df.iterrows()
+        head = Node(None)
+        for i, brand in brand_list:
+            kor_name = brand['kor_name']
+            current_node = head
+            kor_name_jamo = self.return_jamo(kor_name)
+            for kor in kor_name_jamo:
+                if kor not in current_node.children:
+                    current_node.children[kor] = Node(kor)
                 current_node.children[kor].data.append(kor_name)
-            current_node = current_node.children[kor]
-    return head
+                current_node = current_node.children[kor]  
+            
+            kor_name = brand['kor_name']
+            current_node = head
+            kor_name_ja = self.return_chosung(kor_name)
+            for kor in kor_name_ja:
+                if kor not in current_node.children:
+                    current_node.children[kor] = Node(kor)
+                if kor_name not in current_node.children[kor].data:
+                    current_node.children[kor].data.append(kor_name)
+                current_node = current_node.children[kor]  
+        self.trie = head
+        return head
+
+    
+    def searchTrie(self, keyword):
+        result = []
+        current_node = self.trie
+        for word in keyword:
+            if not is_hangul_char(word):
+                if word in current_node.children:
+                    current_node = current_node.children[word]
+                else:
+                    result = current_node.data
+                    break
+            else:
+                if is_jamo(word):
+                    if word in current_node.children:
+                        current_node = current_node.children[word]
+                    else:
+                        result = current_node.data
+                        break
+                else:
+                    word_list = self.return_jamo(word)
+                    for w in word_list:
+                        if w in current_node.children:
+                            current_node = current_node.children[w]
+                        else:
+                            result = current_node.data
+                            break
+
+        if len(result) == 0:
+            result = current_node.data
+        result = result[-10:][::-1]
+        return result
+
+
+
+
+                        
+
+
+
+
+# def returnTrie():
+#     df = pd.read_csv("./brands.csv")
+
+#     brand_list = df.iterrows()
+#     head = Node(None)
+#     for i, brand in brand_list:
+#         kor_name = brand["kor_name"]
+#         current_node = head
+#         for kor in kor_name:
+#             if kor not in current_node.children:
+#                 current_node.children[kor] = Node(kor)
+#                 current_node.children[kor].data.append(kor_name)
+#             else:
+#                 current_node.children[kor].data.append(kor_name)
+#             current_node = current_node.children[kor]
+#     return head
 
--- api/router.py
+++ api/router.py
@@ -5,25 +5,33 @@ from sqlalchemy.orm import Session
 from config.database import get_db
 from typing import Union
 from api import crud, autocomplete
+from api.autocomplete import SubjectName
 
 router = APIRouter(prefix="/api/post")
 
 templates = Jinja2Templates(directory="templates")
-head = autocomplete.returnTrie()
+# head = autocomplete.returnTrie()
+subjectName = SubjectName()
+head = subjectName.returnTrie_ver2()
 
 @router.get("/")
-def return_value(q: Union[str, None] = None):
-    print(q)
-    if q is None:
-        return None
-    else:
-        current = head
-        for key in q:
-            if key in current.children:
-                current = current.children[key]
-            else:
-                break
-        return {"brand": current.data if isinstance(current.data, list) else []}
+def return_value(q : Union[str, None] = None):
+    candidate = subjectName.searchTrie(q)
+    return {"brand" : candidate}
+
+# @router.get("/")
+# def return_value(q: Union[str, None] = None):
+#     print(q)
+#     if q is None:
+#         return None
+#     else:
+#         current = head
+#         for key in q:
+#             if key in current.children:
+#                 current = current.children[key]
+#             else:
+#                 break
+#         return {"brand": current.data if isinstance(current.data, list) else []}
 
 @router.get("/list", response_class=HTMLResponse)
 def post_list_html(request: Request, db: Session = Depends(get_db), q: Union[str, None] = None):
